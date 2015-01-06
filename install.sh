49695a10fd444449bba1bb5ba4f29e5f84ef486a
yu-ichiro <yu-ichiro@users.noreply.github.com>
2015-01-06 16:49:45 +0900

    fix install.sh to load submodules

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 ln -sf $HOME/.settings/zsh/.zshenv $HOME/.zshenv
 ln -sf $HOME/.settings/vim/vimrc $HOME/.vimrc
 
-chsh -s $(which zsh) 
+chsh -s $(which zsh)
+cd ~/.settings 
+git submodule init
+git submodule update

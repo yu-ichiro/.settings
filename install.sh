5f04e58e6265d56a1079ef6485b6e955d6f8fcd0
yu-ichiro <yu-ichiro@users.noreply.github.com>
2015-01-08 10:17:14 +0900

    add check logic for install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 ln -sf $HOME/.settings/zsh/.zshenv $HOME/.zshenv
 ln -sf $HOME/.settings/vim/vimrc $HOME/.vimrc
 
-chsh -s $(which zsh)
+[ "$SHELL" != "$(which zsh)" ]&& chsh -s $(which zsh)
 cd $HOME/.settings 
-git submodule init
-git submodule update
+    git submodule init
+    git submodule update
 cd $HOME
 
-mkdir $HOME/.settings.local
+[ ! -f "$HOME/.zshlocal" ]&& echo "#External zsh local settings" >"$HOME/.zshlocal"

92d296dbcf027928d0bfb2977a477c6bdefbd6f5
yu-ichiro <yu-ichiro@users.noreply.github.com>
2015-01-29 20:58:23 +0900

    [Fix] missing echo

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ NOSTACK=1 cd $HOME
 [ ! -e "$HOME/.zshenv" ]&&          printf	"Linking .zshenv..." && ln -sf $HOME/.settings/zsh/.zshenv $HOME/.zshenv && echo "Done."
 [ ! -e "$HOME/.vimrc" ]&&           printf	"Linking .vimrc..." && ln -sf $HOME/.settings/vim/vimrc $HOME/.vimrc && echo "Done."
 
-[ "$SHELL" != "$(which zsh)" ]&&    echo    "Changing default shell to zsh..." && chsh -s $(which zsh) && "Done."
+[ "$SHELL" != "$(which zsh)" ]&&    echo    "Changing default shell to zsh..." && chsh -s $(which zsh) && echo "Done."
 NOSTACK=1 cd $HOME/.settings;       printf	"Initializeing & Updating Submodules..."
     git submodule update --recursive --init
 NOSTACK=1 cd $HOME;                 echo    "Done."
 
-[ ! -f "$HOME/.zshlocal" ]&&        echo    "Creating .zshlocal..." && echo "#External zsh local settings" >"$HOME/.zshlocal" && "Done."
+[ ! -e "$HOME/.zshlocal" ]&&        printf  "Creating .zshlocal..." && echo "#External zsh local settings" >"$HOME/.zshlocal" && echo "Done."
 printf "Loading .zshenv..."&& source $HOME/.settings/zsh/.zshenv && echo "Done."
 printf "Loading .zshrc..." && source $HOME/.settings/zsh/.zshrc && echo "Done."
 echo

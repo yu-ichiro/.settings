b02a435ca275270ccd422d1f7809057a8e6205fe
yu-ichiro <yu-ichiro@users.noreply.github.com>
2015-01-29 20:43:54 +0900

    [Add] logs to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-# /bin/sh
-cd $HOME
-[ -e "$HOME/.settings" ]|| git clone git@github.com:yu-ichiro/.settings
+# /bin/zsh
+NOSTACK=1 cd $HOME
+[ ! -e "$HOME/.settings" ]&&        echo    "Cloning .settings..." && git clone git@github.com:yu-ichiro/.settings && echo "Done."
 
-[ -e "$HOME/.zshenv" ]|| ln -sf $HOME/.settings/zsh/.zshenv $HOME/.zshenv
-[ -e "$HOME/.vimrc" ]|| ln -sf $HOME/.settings/vim/vimrc $HOME/.vimrc
+[ ! -e "$HOME/.zshenv" ]&&          printf	"Linking .zshenv..." && ln -sf $HOME/.settings/zsh/.zshenv $HOME/.zshenv && echo "Done."
+[ ! -e "$HOME/.vimrc" ]&&           printf	"Linking .vimrc..." && ln -sf $HOME/.settings/vim/vimrc $HOME/.vimrc && echo "Done."
 
-[ "$SHELL" != "$(which zsh)" ]&& chsh -s $(which zsh)
-cd $HOME/.settings 
+[ "$SHELL" != "$(which zsh)" ]&&    echo    "Changing default shell to zsh..." && chsh -s $(which zsh) && "Done."
+NOSTACK=1 cd $HOME/.settings;       printf	"Initializeing & Updating Submodules..."
     git submodule update --recursive --init
-cd $HOME
+NOSTACK=1 cd $HOME;                 echo    "Done."
 
-[ ! -f "$HOME/.zshlocal" ]&& echo "#External zsh local settings" >"$HOME/.zshlocal"
+[ ! -f "$HOME/.zshlocal" ]&&        echo    "Creating .zshlocal..." && echo "#External zsh local settings" >"$HOME/.zshlocal" && "Done."
+printf "Loading .zshenv..."&& source $HOME/.settings/zsh/.zshenv && echo "Done."
+printf "Loading .zshrc..." && source $HOME/.settings/zsh/.zshrc && echo "Done."
+echo
+echo "All Green! Your working environment is complete."
